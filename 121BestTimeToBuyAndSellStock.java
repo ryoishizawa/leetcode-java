43cbcdf21ddbd0d487fb0d6fb58110bd2275bf53
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-02-29 21:24:47 +0800

    Update and rename 121BestTimeToBuyAndSellStockV1.java to 121BestTimeToBuyAndSellStock.java

--- 121BestTimeToBuyAndSellStock.java
+++ 121BestTimeToBuyAndSellStock.java
@@ -14,3 +14,20 @@ class Solution {
         return maxValue;
     }
 }
+
+// Single pass approach
+// Time Complexity: O(n), Space Complexity: O(1)
+class SolutionV2 {
+    public int maxProfit(int[] prices) {
+        int minValue = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (minValue > prices[i]) {
+                minValue = prices[i];
+            } else if (prices[i] - minValue > maxProfit) {
+                maxProfit = prices[i] - minValue;
+            }
+        }
+        return maxProfit;
+    }
+}
