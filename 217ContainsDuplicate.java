59d73c5807f9731110c04ad2e54f7f819de2ada6
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-02-29 20:10:06 +0800

    Create 217ContainsDuplicate.java

---
+++ 217ContainsDuplicate.java
@@ -0,0 +1,16 @@
+// LeetCode 217. Contains Duplicate
+// HashTable Approach - Time: O(n), Space: O(n)
+// Better Approach: Use HashSet to make it simpler (No need to store index in HashMap)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // key: number, value: index
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
