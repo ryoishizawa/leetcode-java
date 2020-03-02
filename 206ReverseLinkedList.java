ebe3b3a60d80f6b34a05d7aad3dde1f6de209153
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-03-02 22:35:55 +0800

    Update 206ReverseLinkedList.java

--- 206ReverseLinkedList.java
+++ 206ReverseLinkedList.java
@@ -8,6 +8,20 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
+
+class Solution2 {
+    public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
